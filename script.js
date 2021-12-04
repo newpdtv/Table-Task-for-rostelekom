7ad886cb711ebab7acedfe0af1c86a14ff46ff4c
Aristov Kirill <95126170+newpdtv@users.noreply.github.com>
2021-12-04 18:05:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
 class Table {
-	constructor(data, element, addRowMassive) {
+	constructor(data, element, addRowMassive, updateRowMassive) {
 		this.data = data
 		this.element = element
 		//addRowMassive - массив с данными, которые передаются в метод AddRow
 		this.addRowMassive = addRowMassive
+		/*updateRowMassive - массив с данными, 
+		которые передаются в метод AddRow,чтобы изменить текст строки
+		Пока что не задействован*/
+		this.updateRowMassive = updateRowMassive
+	}
+	AddRow(element, addRowMassive, updateRowMassive) {
+		let Table = this.element;
+		//создание новых ячеек, столбцов, их заполнение
+		this.addRowMassive.forEach(emp => {
+        let new_row = document.createElement('tr');
+        Object.values(emp).forEach(text => {
+        let cell = document.createElement('td');
+        let textNode = document.createTextNode(text);
+        cell.appendChild(textNode);
+        new_row.appendChild(cell);	
+        	})
+
+       	/*Фактически не работает, вернее работает совершенно неправильно
+       	 это просто чтобы хоть что-то было
+       	 Не аффектится с Data*/
+        let update_button = document.createElement("button")
+        update_button.innerHTML = "Update"
+        update_button.onclick = function() {
+        	let updateRowMassive = ["123412341234"]
+        	let updating_row = new_row.closest("tr")
+        	updating_row.innerHTML = updateRowMassive[0]
+        }
+        new_row.appendChild(update_button)
+
+        //создание кнопки с функцией, которая удаляет ближайший к ней new_row
+        let delete_button = document.createElement("button")
+        delete_button.innerHTML = "Delete"
+        delete_button.onclick = function() {
+        	new_row.closest("tr").remove()
+        }
+        new_row.appendChild(delete_button)
+        Table.appendChild(new_row);
+        })
 	}
 	RenderTable(data, element) {
 		let myTable = this.element;
@@ -28,31 +66,25 @@ class Table {
         cell.appendChild(textNode);
         row.appendChild(cell);
         })
+
+        //кнопки, удаляющие строки, сформерованные из массива data
+        let deleteData_button = document.createElement("button")
+        deleteData_button.innerHTML = "Delete"
+        deleteData_button.onclick = function() {
+        	row.closest("tr").remove()
+        }
+        row.appendChild(deleteData_button)
         table.appendChild(row);
     });
-    myTable.appendChild(table);
+
+    	/*Цикл, удаляющий таблицу, если она есть, не аффектится на строки, добавленные через addRow,
+		поэтому, если попытаться вызвать после добавления новых строк, удалит только новые строки
+		из старой таблицы и создаст новую рядом со старой*/
+    	if (myTable.lastChild) {
+    		myTable.removeChild(myTable.lastChild)
+    	}
+    	myTable.appendChild(table);
 	};
-	AddRow(element, addRowMassive) {
-		let Table = this.element;
-		//создание новых ячеек, столбцов, их заполнение
-		this.addRowMassive.forEach(emp => {
-        let new_row = document.createElement('tr');
-        Object.values(emp).forEach(text => {
-        let cell = document.createElement('td');
-        let textNode = document.createTextNode(text);
-        cell.appendChild(textNode);
-        new_row.appendChild(cell);	
-        	})
-        //создание кнопки с функцией, которая удаляет ближайший new_row
-        let delete_button = document.createElement("button")
-        delete_button.innerHTML = "Delete"
-        delete_button.onclick = function() {
-        	new_row.closest("tr").remove()
-        }
-        new_row.appendChild(delete_button)
-        Table.appendChild(new_row);
-        })
-	}
 }
 
 let myMainTable = new Table(
@@ -60,24 +92,30 @@ let myMainTable = new Table(
 		{Pet: 'Собака', Name: 'Тузик'},
 		{Pet: 'Собака', Name: 'Барон'},
 		{Pet: 'Кот', Name: 'Мурзик'}
-	], document.getElementById('container-table-with-init-data'), 
+	], 
+	document.getElementById('container-table-with-init-data'), 
 	[{Pet: "Птица", Name: "Антон"}]);
 
 //кнопки, вызывающие render и addrow методы
-let  render_button = document.createElement( "button" )
-const text = document.createTextNode( "Render" )
-render_button.appendChild( text )                  
-document.body.appendChild( render_button )
+let render_button = document.createElement("button")
+const text = document.createTextNode("Render")
+render_button.appendChild(text)                  
+document.body.appendChild(render_button)
 
 render_button.onclick = function() {
 	myMainTable.RenderTable()
 }
 
-let add_button = document.createElement( "button" )
-const text_for_add = document.createTextNode( "Add Row" )
+let add_button = document.createElement("button")
+const text_for_add = document.createTextNode("Add Row")
 add_button.appendChild(text_for_add)                  
 document.body.appendChild(add_button)
 
 add_button.onclick = function() {
 	myMainTable.AddRow()
 }
+
+document.body.style.margin = '20px';
+    document.body.style.margin;
+    document.body.style.marginTop; 
+    document.body.style.marginLeft; 
